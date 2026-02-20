bd0312a554ec330db11df56944fd3c0038c7a708
localhost433 <tc4119@nyu.edu>
2026-02-19 21:29:19 -0500

    Added solution - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations_1808772186.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String op: operations) {
+            x += (44 - op.charAt(1));
+        }
+        return x;
+    }
+}
