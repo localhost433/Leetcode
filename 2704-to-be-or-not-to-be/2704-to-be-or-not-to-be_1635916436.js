a20d3c3a0a6da29dcda0f62752a61ee417e84d6f
localhost433 <tc4119@nyu.edu>
2026-02-19 21:30:57 -0500

    Added solution - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be_1635916436.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    let object = {
+        toBe: function(arg1) {
+            if (val === arg1) return true;
+            else throw new Error("Not Equal");
+        },
+        notToBe: function(arg2) {
+            if (val !== arg2) return true;
+            throw new Error("Equal");
+        }
+    };
+    return object
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
